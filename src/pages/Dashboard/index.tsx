c496afecfed48a68dfeabae20b354e064bc27993
Artur Paranayba <artur.paranayba@gmail.com>
2020-06-24 16:48:51 -0300

    added 'route bug fix

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -92,7 +92,7 @@ const Dashboard: React.FC = () => {
         {repositories.map((repository) => (
           <Link
             key={repository.full_name}
-            to={`GithubExplorer/repositories/${repository.full_name}`}
+            to={`repositories/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
