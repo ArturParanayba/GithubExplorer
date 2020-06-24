fb3c00fecfbd5a8b19b0b6608efd4b8ec57cd8a8
Artur Paranayba <artur.paranayba@gmail.com>
2020-06-23 21:21:43 -0300

    Updates

---
+++ precache-manifest.4e260d653deaf6ad17546887c4cf71aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbe3a469df0f7be2156995f3d8cac56",
+    "url": "/GithubExplorer/index.html"
+  },
+  {
+    "revision": "ecdd3bdaf6d66684b067",
+    "url": "/GithubExplorer/static/js/2.ccb20935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GithubExplorer/static/js/2.ccb20935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa87697e86d643e47e4c",
+    "url": "/GithubExplorer/static/js/main.f590122b.chunk.js"
+  },
+  {
+    "revision": "65d034e78d6e0fcdb657",
+    "url": "/GithubExplorer/static/js/runtime-main.1ecce92d.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/GithubExplorer/static/media/github-logo.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/GithubExplorer/static/media/logo.04f6798f.svg"
+  }
+]);
